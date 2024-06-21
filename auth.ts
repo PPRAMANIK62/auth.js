b79b3f3ae7788a02bb15828368ed7df6f0bcce23
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-06-21 19:24:17 +0530

    handling callbacks in auth.ts

--- auth.ts
+++ auth.ts
@@ -1,7 +1,25 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import NextAuth from "next-auth";
+import NextAuth, { type DefaultSession } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import authConfig from "./auth.config";
+import { getUserById } from "./data/user";
 import { db } from "./lib/db";
+
+// fixing the typescript error for session.user.role
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      role: "ADMIN" | "USER";
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    role?: "ADMIN" | "USER";
+  }
+}
 
 export const {
   auth,
@@ -9,6 +27,33 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async signIn({ user }) {
+      const existingUser = await getUserById(user.id!);
+      if (!existingUser || !existingUser.emailVerified) return false;
+
+      return true;
+    },
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
