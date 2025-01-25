39209a08019fb908027e9183a36b11685c6bbbbc
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2025-01-25 23:09:36 +0530

    feat: credentials login working

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,6 +1,9 @@
 "use server";
 
 import { LoginSchema } from "@/schemas";
+import { signIn } from "@/server/auth/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/server/auth/routes";
+import { AuthError } from "next-auth";
 import { z } from "zod";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
@@ -8,6 +11,28 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
 
   if (!validatedFields.success) {
     return { error: "Invalid fields!" };
+  }
+
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    console.error(error);
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials!" };
+        default:
+          return { error: "Something went wrong!" };
+      }
+    }
+
+    throw error;
   }
 
   return { success: "Email sent!" };
--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,9 +1,21 @@
-import { auth } from "@/server/auth/auth";
+import { auth, signOut } from "@/server/auth/auth";
 
 const SettingsPage = async () => {
   const session = await auth();
 
-  return <div>{JSON.stringify(session)}</div>;
+  return (
+    <div>
+      {JSON.stringify(session)}
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button type="submit">Log Out</button>
+      </form>
+    </div>
+  );
 };
 
 export default SettingsPage;
