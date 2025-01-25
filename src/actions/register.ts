18b692fe8f59fa0657e9e7df6719b2a6f8b5a99e
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2025-01-25 18:28:18 +0530

    db: register using email working

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,6 +1,10 @@
 "use server";
 
+import { getUserByEmail } from "@/data/user";
+import { env } from "@/env";
 import { RegisterSchema } from "@/schemas";
+import { db } from "@/server/db";
+import bcrypt from "bcryptjs";
 import { z } from "zod";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -9,6 +13,27 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
+
+  const {email, name, password} = validatedFields.data;
+
+  const salt = await bcrypt.genSalt(Number(env.SALT_ROUNDS));
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
 
   return { success: "Email sent!" };
 };
