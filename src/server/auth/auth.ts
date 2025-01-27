8a991e34d5e95c808de2a8165065e8fc077d57d6
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2025-01-27 10:41:27 +0530

    feat: email verified enabled for OAuth

--- src/server/auth/auth.ts
+++ src/server/auth/auth.ts
@@ -1,5 +1,6 @@
 import { getUserById } from "@/data/user";
 import authConfig from "@/server/auth/auth.config";
+import { db } from "@/server/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient, UserRole } from "@prisma/client";
 import NextAuth from "next-auth";
@@ -7,6 +8,18 @@ import NextAuth from "next-auth";
 const prisma = new PrismaClient();
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error",
+  },
+  events: {
+    linkAccount: async ({ user }) => {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
+    },
+  },
   callbacks: {
     // signIn: async ({ user }) => {
     //   const existingUser = await getUserById(user.id!);
